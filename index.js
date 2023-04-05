1b6a819a6cc62efcdd171e4e9c03e42990997ae3
Erickkirui <erickkirui653@gmail.com>
2023-04-05 12:53:21 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      // If the sum of the pair equals the target, return true
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  // If no pair adds up to the target, return false
+  return false;
+  }
+ 
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity of the hasTargetSum function is O(n^2), where n is the length of the input array. 
+  This is because the function uses nested loops to iterate through all pairs of numbers in the array, 
+  which results in a quadratic time complexity. Specifically, the outer loop runs n times, and the inner loop runs (n-1), (n-2), ..., 2, and 1 times 
+  for each iteration of the outer loop. Therefore, the total number of iterations is 1 + 2 + ... + (n-1), 
+  which is equal to (n^2 - n) / 2. Since the (n^2) term dominates as n gets larger, the time complexity is O(n^2).
 */
 
 /* 
   Add your pseudocode here
+  function hasTargetSum(arr, target):
+    for i from 0 to arr.length - 1:
+        for j from i + 1 to arr.length - 1:
+            if arr[i] + arr[j] == target:
+                return true
+    return false
 */
 
 /*
